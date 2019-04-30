3eae9192c42fd95486a3461edb67d76e272f0414
ObsidianDestroyer <rastaintman@hotmail.com>
2019-04-30 15:17:56 +0300

    Набросок стримера, имеет метод возвращающий стрим цен

--- src/oanda/streamer.py
+++ src/oanda/streamer.py
@@ -1,4 +1,25 @@
+from src.params.configure import Config
+
 from oandapyV20 import API
+from oandapyV20.endpoints.pricing import PricingStream
 
 
-class Streamer
+params = {
+    "instruments": "EUR_USD"
+}
+
+
+class Streamer:
+    api = API(
+        access_token=Config.get_api_token(),
+        environment='practice'
+    )
+
+    stream = PricingStream(
+        accountID=Config.get_account_id(),
+        params=params
+    )
+
+    def start_stream(self):
+        rv = self.api.request(self.stream)
+        return rv
