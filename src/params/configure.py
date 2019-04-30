f7901e238f0355b4359cf2c4c36425f5ec866d64
ObsidianDestroyer <rastaintman@hotmail.com>
2019-04-30 15:18:24 +0300

    Написал конфигуратор

--- src/params/configure.py
+++ src/params/configure.py
@@ -1,2 +1,40 @@
+from yaml import Loader, load
+from os import listdir
+from glob import glob
 
+from datetime import datetime, timedelta
+
+config_path = glob('configuration.yml').pop(0)
+
+
+def get_config():
+    if config_path in listdir():
+        data_loaded = load(open(config_path))
+        return data_loaded
+
+
+class Config:
+    def __init__(self, *file):
+        self.file = file
+
+    @staticmethod
+    def get_current_time(**offset: str) -> str:
+        """
+        Standard offset is 10 minutes from now
+        """
+        if offset:
+            return datetime.strftime((datetime.now() + timedelta(minutes=offset)), '%H:%M')
+
+        # default offset
+        return datetime.strftime((datetime.now() + timedelta(minutes=10)), '%H:%M')
+
+    @staticmethod
+    def get_api_token():
+        f = get_config()
+        return f['token']
+
+    @staticmethod
+    def get_account_id():
+        f = get_config()
+        return f['active_account']
 
