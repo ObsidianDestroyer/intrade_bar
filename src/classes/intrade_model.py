ba6abfa81b8c59fff70e25dbdf39491beecdfe54
ObsidianDestroyer <rastaintman@hotmail.com>
2019-04-30 15:17:29 +0300

    Переименовал intrade_class в intrade_model

--- src/classes/intrade_model.py
+++ src/classes/intrade_model.py
@@ -1,3 +1,5 @@
+from src.params.configure import Config
+
 from datetime import datetime, timedelta
 
 
@@ -8,7 +10,7 @@ class IntradeBarModel:
 
     @property
     def user_hash(self) -> str:
-        return '14a6ab43cca40185cf41cc3b2a94937f'
+        return Config.get_api_token()
 
     @property
     def option(self) -> str:
@@ -18,16 +20,9 @@ class IntradeBarModel:
     def investment(self) -> str:
         return 95
 
-    @staticmethod
-    def get_current_time(self, **offset: int) -> str:
-        """
-        Standart offset is 10 minutes from now
-        """
-        if offset:
-            return datetime.strftime((datetime.now() + timedelta(minutes=offset)), '%H:%M')
-
-        # default offset
-        return datetime.strftime((datetime.now() + timedelta(minutes=10)), '%H:%M')
+    @property
+    def time(self):
+        return Config.get_current_time()
 
     @property
     def date(self) -> str:
